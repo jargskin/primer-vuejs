711191d6507bb052520153733269292cb4ece217
Jordan rodriguez <j.rodriguez@smaas.org>
2018-08-31 00:42:39 -0300

    primera parte curso

--- codigo/js/main.js
+++ codigo/js/main.js
@@ -1,1 +1,55 @@
-alert('holamundo');
+Vue.filter('mayusculas', (value) => value.toUpperCase());
+new Vue({
+    el: 'main',
+    mounted() {
+       axios
+            .get('https://mindicador.cl/api')
+            .then((respuesta) => {
+               this.indicadores = respuesta.data;
+            });
+        
+    },
+    data: {
+        indicadores: null,
+        texto: 'Hola Mundo desde vue2',
+        nombre: 'Jordan',
+        apellido: 'Rodirguez',
+        nota: 5,
+        peliculas: ['Batman vs superman', 'AA', 'la verdad duele', 'los mercenarios', 'spiderman', 'Catastrofe in the hell'],
+        frutas: [
+            { nombre: 'manzana',temporada: 'Invierno',precio: 'bajo' },
+            { nombre: 'naranja',temporada: 'otoño',precio: 'medio' },
+            { nombre: 'cereza',temporada: 'Primavera',precio: 'Alto' },
+            { nombre: 'Sandia',temporada: 'Verano',precio: 'Medio' }
+        ],
+        superfruta: { nombre: 'Sandia', temporada: 'Verano', precio: 'Medio' },
+        peliculaNueva: null,
+        busqueda: null,
+        confirmado: null
+    },
+    methods: {
+        crearPelicula(){
+            this.peliculas.unshift(this.peliculaNueva);
+            this.peliculaNueva = null;
+        },
+        borrarPelicula(indice){
+            this.peliculas.splice(indice, 1);
+        },
+        marcar(index){
+            this.confirmado = index;
+        }
+    },
+    computed: {
+        nombreYapellidos(){
+            let date = new Date();
+            let year = date.getFullYear();
+            return this.nombre+" "+this.apellido+" - Nota: "+this.nota+" "+year+"/"+date.getMonth()+"/"+date.getDay();
+        },
+        peliculasOrdenadas(){
+            return this.peliculas.sort();
+        },
+        buscarFruta(){
+            return this.frutas.filter( (fruta) => fruta.nombre.includes(this.busqueda) )
+        }
+    }
+});
